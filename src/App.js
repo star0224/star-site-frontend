ca5c1eca3cf6521a3d908f0f8cc5ff528fb8e7b4
star0224 <1048950702@qq.com>
2019-12-03 15:23:59 +0800

    introduce antd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
+import {Navigation} from "./components";
+import 'antd/dist/antd.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <Navigation/>
+    );
 }
 
 export default App;
